9ca9a2a52f9ce4343eb6eb53ef8e2d598fd2d314
S12C09 <S12C09@S12C09>
2024-12-07 15:04:27 +0330

    Add project files.

---
+++ MostatilNew/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostatilNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tool ra vared konid :");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("arz ra vared konid :");
+            double b = double.Parse(Console.ReadLine());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
